9aa90ce391ac56810fcf42f57caddb6610545cc4
ggk7015 <ylerk7015@gmail.com>
2026-09-24 18:49:23 +0800

    auto: update code

--- src/main/java/io/github/ggk7015/ggk7015mod/ModItems.java
+++ src/main/java/io/github/ggk7015/ggk7015mod/ModItems.java
@@ -2,4 +2,5 @@ package io.github.ggk7015.ggk7015mod.item.GocSItem;
 
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
-import net.minecraft.
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
